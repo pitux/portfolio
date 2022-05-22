ec8e838410984ac9169db632b3d56dc1c28b0879
Pitux <padanguina@gmail.com>
2022-05-22 12:31:42 -0300

    Add new Project

--- src/app/components/model/project.ts
+++ src/app/components/model/project.ts
@@ -1,11 +1,23 @@
-export interface Project {
+export class Project {
 
-    project_id: any;
-    project_name: string;
-    project_description: string;
-    project_tools: string;
-    project_start: string;
-    project_snapshot: string;
-    project_url: string;
+    project_id!: number;
+    project_name!: string;
+    project_description!: string;
+    project_tools!: string;
+    project_start!: string;
+    project_snapshot!: string;
+    project_url!: string;
+
+
+    constructor (
+        project_name: string,
+        project_description: string,
+        project_tools: string,
+        project_start: string,
+        project_snapshot: string,
+        project_url: string
+    ){
+
+    }
 
 }
--- src/app/components/projects/new-projects/new-projects.component.ts
+++ src/app/components/projects/new-projects/new-projects.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { TokenService } from 'src/app/security/service/token.service';
+import { ProjectsService } from 'src/app/service/projects.service';
+import { Project } from '../../model/project';
 
 @Component({
   selector: 'app-new',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewProjectComponent implements OnInit {
 
-  constructor() { }
+  
+  roles!: string[];
+  isAdmin = false;
+
+  project_id!: number;
+  project_name!: string;
+  project_description!: string;
+  project_tools!: string;
+  project_start!: string;
+  project_snapshot!: string;
+  project_url!: string;
+
+
+  constructor(
+    private projectService: ProjectsService,
+    private toastr: ToastrService,
+    private tokenService: TokenService
+  ) { }
 
   ngOnInit(): void {
+  } 
+  
+  onCreate(): void {
+    const project = new Project(this.project_name, this.project_description, this.project_start, this.project_tools, this.project_snapshot, this.project_url);
+    this.projectService.saveProject(project).subscribe(
+      data => {
+        this.toastr.success('Producto Creado', 'OK', {
+          timeOut: 3000, positionClass: 'toast-top-center'
+        });
+        //this.router.navigate(['/lista']);
+      },
+      err => {
+        this.toastr.error(err.error.mensaje, 'Fail', {
+          timeOut: 3000,  positionClass: 'toast-top-center',
+        });
+        // this.router.navigate(['/']);
+      }
+    );
   }
 
 }
